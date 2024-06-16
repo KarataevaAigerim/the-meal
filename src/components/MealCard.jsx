2c40633d2f1c10fce7a5aed4a243764cb42a4453
aigerimkarataeva <ai.gerim@hotmail.com>
2024-06-16 17:01:49 +0600

    fixed the flex position on meal-block

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const MealCard = ({ meal }) => (
     <div className="meal-block">
-    <Link to={`/meal/${meal.idMeal}`}>
-        <img src={meal.strMealThumb} alt={meal.strMeal} />
-        <div className='meal-block-text'>
-            <h3>{meal.strMeal}</h3>
-            <p>{meal.strArea}/{meal.strCategory}</p>
-        </div>
-        
-    </Link>
+        <Link to={`/meal/${meal.idMeal}`} className='meal-link'>
+            <img src={meal.strMealThumb} alt={meal.strMeal} />
+            <div className='meal-block-text'>
+                <h3>{meal.strMeal}</h3>
+                <p>{meal.strCategory} | {meal.strArea}</p>
+            </div>
+            
+        </Link>
   </div>
 );
 
--- src/components/MealOfTheDay.jsx
+++ src/components/MealOfTheDay.jsx
@@ -6,7 +6,7 @@ const MealOfTheDay = ({ meal }) => (
     <div className='random-meal-block-text'>
         <h2>Meal of the Day</h2>
         <h1 className="title"><Link to={`/meal/${meal.idMeal}`}>{meal.strMeal}</Link></h1>
-        <p>{meal.strArea} | {meal.strCategory}</p>
+        <p>{meal.strCategory} | {meal.strArea}</p>
     </div>
     <div className='random-meal-block-img'>
         <img src={meal.strMealThumb} alt={meal.strMeal} />
